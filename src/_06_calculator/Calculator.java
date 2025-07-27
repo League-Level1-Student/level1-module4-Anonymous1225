25f839459054fc248c3fbd015aab557479b7af2b
league <league@imac-20.attlocal.net>
2025-07-26 17:57:28 -0700

    hehe i did nothin

--- src/_06_calculator/Calculator.java
+++ src/_06_calculator/Calculator.java
@@ -1,53 +1,78 @@
 package _06_calculator;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class Calculator {
+public class Calculator implements ActionListener {
 	JTextField winnie;
 	JTextField waffle;
+	JButton gigi = new JButton("Add");
+	JButton martha = new JButton("Subtract");
+	JButton Polly = new JButton("Subtract");
 	public Calculator() {
 		int op=0;
 		winnie = new JTextField();
 		waffle = new JTextField();
 		JFrame beth = new JFrame();
 		JPanel polly = new JPanel();
-		JButton gigi = new JButton("Add");
-		JButton martha = new JButton("Subtract");
+
 
 		beth.add(polly);
 		polly.add(winnie);
 		polly.add(gigi);
 		polly.add(waffle);
-		gigi.addActionListener(e->System.out.println(add(iv1,iv2)));
+		gigi.addActionListener(this);
 		polly.add(martha);
-		martha.addActionListener(e-> System.out.println(subtract(iv1,iv2)));
+		martha.addActionListener(this);
 		beth.pack();
 		beth.setVisible(true);
 
 	}
-	public static int add(int x, int y){
-		String v1=winnie.getText();
-		String v2=waffle.getText();
-		int iv1 = Integer.parseInt(v1);
-		int iv2 = Integer.parseInt(v2);
+	public  int add(int x, int y){
 		int sum=x+y;
 		return sum;
 		
 	}
-	public static int multiply(int p1, int p2) {
+	public  int multiply(int p1, int p2) {
 		int answer=0;
 		for(int i = 0; i < p2; i++) {
 			add(answer, p1);
 		}
 		return answer;
 	}
-	public static int subtract(int x, int y) {
+	public  int subtract(int x, int y) {
 		int subber=y*-1;
 		int answer=x+subber;
 		return answer;
 	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		JButton buttonPressed = (JButton) e.getSource();
+		if (buttonPressed.equals(gigi)){
+			System.out.println("HIIIIII");
+			String v1=winnie.getText();
+			String v2=waffle.getText();
+			int iv1 = Integer.parseInt(v1);
+			int iv2 = Integer.parseInt(v2);
+			int sun= add(iv1,iv2);
+			System.out.println(sun);
+		}
+	}
 }
+
+
+
+
+
+
+
+
+
+
