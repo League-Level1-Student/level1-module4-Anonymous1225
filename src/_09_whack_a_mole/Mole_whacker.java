c1e730c6968bacc4ebdc60592324315d357c9a51
league <league@imac-20.attlocal.net>
2025-09-20 16:28:50 -0700

    e

--- src/_09_whack_a_mole/Mole_whacker.java
+++ src/_09_whack_a_mole/Mole_whacker.java
@@ -1,11 +1,33 @@
 package _09_whack_a_mole;
 
+import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class Mole_whacker {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Mole mole = new Mole();
-		
+		Random ran = new Random();
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		JButton [] Bobs = new JButton[24];
+		frame.setVisible(true);
+		for (int i=0; i<Bobs.length;i++) {
+			Bobs[i]=new JButton();
+			Bobs[i].addActionListener(null);
+			panel.add(Bobs[i]);
+		}
+		frame.add(panel);
+		for (int i=0; i<Bobs.length; i++) {
+			panel.add(Bobs[i]);
+		}
+		frame=new JFrame();
+		frame.setSize(300,300);
+		int X= ran.nextInt(23)+1;
+		Bobs[X].setText("Mole!");
 	}
 
 }
