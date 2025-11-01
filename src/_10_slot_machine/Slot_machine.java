e89fb1d91e51356715af96c63eb0aa9b759a3722
league <maddoxseul@sbsd.net>
2025-11-01 16:28:52 -0700

    ghp_nwQqF

--- src/_10_slot_machine/Slot_machine.java
+++ src/_10_slot_machine/Slot_machine.java
@@ -1,30 +1,37 @@
-
-
 package _10_slot_machine;
 
+import java.awt.Component;
 import java.net.*;
 import java.util.Random;
 
 import javax.swing.*;
 
 public class Slot_machine {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws MalformedURLException {
 		Slot_machine bandit = new Slot_machine();
+		bandit.run();
 	}
 	public void run() throws MalformedURLException {
 		Random ran = new Random();
 		JFrame frame = new JFrame();
 		JPanel panel = new JPanel();
 		frame.add(panel);
+		frame.setVisible(true);
+		Component compy= new Component();
 		for (int i=0; i<3;i++) {
 			int x = ran.nextInt(3);
 			if(x==0) {
-				createLabelImage("6509378.png");
+				compy=panel.add(createLabelImage("650938.png"));
 			}
 			if(x==1) {
-				createLabelImage("7.jpg");
+				compy=createLabelImage("7.jpg");
+			}
+			if(x==2) {
+				compy=panel.add(createLabelImage("bar.jpg"));
 			}
 		}
+		panel.add(compy);
+		
 	}
 	private JLabel createLabelImage(String fileName) throws MalformedURLException{
         URL imageURL = getClass().getResource(fileName);
